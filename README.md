d91492b978639a55a30ac73b6991a0a057f14f60
Oleg kreilt <pogodin-olezhka@inbox.ru>
2026-08-06 18:39:27 +0300

    first migration and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# Monster tracker API
+Данный проект наследник проекта monster-energy-tracker-collection, который был написан на html/css/js с участием firebase.
+Цель: переписать backend на Golang с использованием PostgreSQL, получить собственный сервис. 
---
+++ migrations/00001_init_schema.sql
@@ -0,0 +1,42 @@
+-- +goose Up
+
+CREATE TYPE rarity AS ENUM ('ordinary','unusual','rare','very rare','legendary');
+
+CREATE TYPE statuses AS ENUM ('dist','lim','new','reg','alc');
+
+CREATE TABLE flavors
+(flavor_id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+title text NOT NULL,
+lineup text NOT NULL,
+description text,
+rare rarity NOT NULL,
+region varchar(50) NOT NULL,
+color varchar(10) NOT NULL,
+status statuses NOT NULL,
+photo text
+);
+
+CREATE TABLE users
+(user_id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+external_id text NOT NULL UNIQUE,
+nickname text,
+email text NOT NULL
+);
+
+CREATE TABLE flavors_users
+(flavor_user_id integer GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+user_id integer NOT NULL REFERENCES users(user_id) ON DELETE CASCADE,
+flavor_id integer NOT NULL REFERENCES flavors(flavor_id),
+status_tried boolean NOT NULL,
+tried_at timestamptz,
+user_photo text,
+UNIQUE (user_id, flavor_id)
+);
+
+-- +goose Down
+
+DROP TABLE flavors_users;
+DROP TABLE users;
+DROP TABLE flavors;
+DROP TYPE statuses;
+DROP TYPE rarity;
