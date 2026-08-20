1eb1f35bdf4983d5f8cacaf3764fe97afb79bc03
Oleg kreilt <pogodin-olezhka@inbox.ru>
2026-08-20 23:47:09 +0300

    add firebase admin sdk initialization

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -9,15 +9,22 @@ import (
 	"syscall"
 	"time"
 
+	firebase "firebase.google.com/go/v4"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/kreilt/monster-tracker/internal/handlers"
 	"github.com/kreilt/monster-tracker/internal/repository"
+	"google.golang.org/api/option"
 )
 
 func main() {
 	dbURL := os.Getenv("MONSTER_DB")
 	if dbURL == "" {
 		log.Fatal("MONSTER_DB is not set")
+	}
+
+	credPath := os.Getenv("FIREBASE_CREDENTIALS")
+	if credPath == "" {
+		log.Fatal("FIREBASE_CREDENTIALS is not set")
 	}
 
 	startCtx := context.Background()
@@ -32,6 +39,18 @@ func main() {
 	if err := pool.Ping(startCtx); err != nil {
 		log.Fatalf("unable to reach database: %v", err)
 	}
+
+	opt := option.WithAuthCredentialsFile(option.ServiceAccount, credPath)
+	app, err := firebase.NewApp(startCtx, nil, opt)
+	if err != nil {
+		log.Fatalf("error initializing app: %v", err)
+	}
+
+	client, err := app.Auth(startCtx)
+	if err != nil {
+		log.Fatalf("error getting Auth client: %v", err)
+	}
+	_ = client
 
 	flavorsRepo := repository.NewFlavor(pool)
 
